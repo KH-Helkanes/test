f3fb6f5f031b25a8ffef0b848cc8bb787c5c01ea
kangk <kangk@DESKTOP-3JSJ1AB>
2020-05-18 17:15:12 +0900

    20051817 4th commit

--- bGit/src/bGitTest/Test.java
+++ bGit/src/bGitTest/Test.java
@@ -5,6 +5,12 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("�ȳ�");
 		System.out.println("�ȳ� �ݰ��� �� �����̾�");
+		
+		System.out.println("yyyyyyyyyyyyyyyyy");
+		
+		System.out.println("�ȳ�");
+		System.out.println("�ȳ� �ݰ��� �� �����̾�");
+		
 		System.out.println("xxxxxxxxxxxxxxx");
 		
 	}
